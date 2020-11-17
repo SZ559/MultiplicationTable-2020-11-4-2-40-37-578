a01e128bd84a1ab14975769e4384e23b23e657aa
Shishi Zhang <SZhang42@slb.com>
2020-11-17 14:36:49 +0800

    feat: add validation to check if start number larger than end number

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,17 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
+
+  public render(startNumber: number, endNumber: number): string{
+    if(!this.isValidInput(startNumber, endNumber)){
+      return ''
+    }
     return '1*1=1'
+  }
+
+  private isValidInput(startNumber: number, endNumber: number): boolean {
+    return this.isEndNumberLargerOrEqualToStartNumber(startNumber, endNumber)
+  }
+
+  private isEndNumberLargerOrEqualToStartNumber(startNumber: number, endNumber: number): boolean {
+    return endNumber >= startNumber
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,9 +1,9 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
+  const table = new MultiplicationTable()
   it('should render multiplication table of (1, 1)', () => {
     // given
-    const table = new MultiplicationTable()
     const start = 1
     const end = 1
 
@@ -12,5 +12,18 @@ describe('Multiplication Table', () => {
 
     //then
     expect(rendered).toBe('1*1=1')
+  })
+
+  it('should return an empty string when start number larger than end number', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 3
+    const end = 2
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
   })
 })
